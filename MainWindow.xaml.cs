0cfe4123c5ea767197739833cdf5f70da482f0e9
S1dechko <slaaaadd@mail.ru>
2022-04-30 15:39:46 +0700

    Грубая готовность, имеется возможность добав/удал/изм запись на опред день. Файл с записями находиться в ПАПКА_ПОЛЬЗОВАТЕЛЯ/kp330/def.xml

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,6 +20,13 @@ namespace WPFkalendarZpisnoy
     /// </summary>
     public partial class MainWindow : Window
     {
+        int selectedYear = 0;
+        int selectedMount = 0;
+        Button[] days = new Button[31];
+        DateTime selectedDateTime;
+        WorkWithFile wwf = new WorkWithFile("def");
+        int selectedNoteId = -1;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -28,21 +35,179 @@ namespace WPFkalendarZpisnoy
 
         private void loadMain(object sender, RoutedEventArgs e)
         {
-            for(int i = 0; i < daysGrid.RowDefinitions.Count; i++)
+            setCurrentDate();
+            generateCalendar();
+            updateSelectedDate();
+            updateSelectedDayInfo(DateTime.Now.Day);
+        }
+
+        private void generateCalendar()
+        {
+            int i = 0;
+            while (i < 31)
             {
-                for (int j = 0; j < daysGrid.ColumnDefinitions.Count; j++)
-                {
-                    int labelContent = j + i * 7 + 1;
-                    if (labelContent > 31) continue;
-                    var label = new Label();
-                    label.Margin = new Thickness(5);
-                    label.Background = new SolidColorBrush(Colors.Aquamarine);
-                    label.Content = labelContent;
-                    daysGrid.Children.Add(label);
-                    Grid.SetRow(label, i);
-                    Grid.SetColumn(label, j);
-                }
+                Button date = new Button();
+                date.Content = i + 1;
+                date.Name = "day" + i;
+                date.Click += clickDate;
+                Grid.SetRow(date, i / 7);
+                Grid.SetColumn(date, i % 7);
+                daysGrid.Children.Add(date);
+                date.Style = (Style)this.Resources["dayButton"];
+                days[i] = date;
+                i++;
             }
+        }
+        
+        private void updateDayCountInMount()
+        {
+            int dayInMonth = DateTime.DaysInMonth(selectedYear, selectedMount);
+            for(int i = 27; i<31; i++)
+            {
+                days[i].Visibility = i < dayInMonth ? Visibility.Visible : Visibility.Hidden;
+            }
+        }
+
+        private void updateWeekend()
+        {
+            int dayInMount = DateTime.DaysInMonth(selectedYear, selectedMount);
+            bool isThisMountAndYear = DateTime.Now.Year == selectedYear && DateTime.Now.Month == selectedMount;
+            int currentDay = DateTime.Now.Day;
+            List<int> _days = wwf.findNumNotedDayAtMY(selectedYear, selectedMount).ToList();
+            for (int i = 0; i< 31; i++)
+            {
+                if (dayInMount < i+1) continue;
+                if(isThisMountAndYear && currentDay == i + 1)
+                {
+                    days[i].Tag = "T";
+                    continue;
+                }
+                DateTime dt = new DateTime(selectedYear, selectedMount, i+1);
+                if (dt.DayOfWeek.Equals(DayOfWeek.Saturday) || dt.DayOfWeek.Equals(DayOfWeek.Sunday))
+                    days[i].Tag = "W";
+                else days[i].Tag = "O";
+            }
+            foreach(int day in _days)
+            {
+                days[day-1].Tag = "N";
+            }
+        }
+
+        private void setCurrentDate()
+        {
+            DateTime now = DateTime.Now;
+            this.selectedYear = now.Year;
+            this.selectedMount = now.Month;
+        }
+
+        private void updateSelectedDate()
+        {
+            SelectedYearLabel.Content = selectedYear;
+            SelectedMountLabel.Content = selectedMount;
+            updateDayCountInMount();
+            updateWeekend();
+        }
+
+        private void clickDate(object sender, RoutedEventArgs e)
+        {
+            if (!(sender is Button)) return;
+            if (!(((Button)sender).Content is int)) return;
+            updateSelectedDayInfo((int)((Button)sender).Content);
+            updateSelectedDayNotes((int)((Button)sender).Content);
+            selectedNoteId = -1;
+        }
+
+        private void updateSelectedDayNotes(int day)
+        {
+            selectedDayNotes.ItemsSource = wwf.findNotesAtDateAsNote(selectedYear, selectedMount, day).OrderBy(n=>n.id).Select(d=>d.name);
+        }
+
+        private void updateSelectedDayInfo(int day)
+        {
+            selectedDateTime = new DateTime(selectedYear, selectedMount, day);
+            selectedDate.Content = selectedDateTime.ToString("dd.MM.yyyy (ddd)");
+        }
+
+        private void moveDate(object sender, RoutedEventArgs e)
+        {
+            if (!(sender is Button)) return;
+            Button button = (Button)sender;
+            if (!(button.Tag is string)) return;
+            string tag = (string)button.Tag;
+            if (!(button.Content is string)) return;
+            string content = (string)button.Content;
+            if (content != "+" && content != "-") return;
+            bool typeOfMove = content == "+";
+            if (tag == "Y") moveYear(typeOfMove);
+            else if (tag == "M") moveMount(typeOfMove);
+            updateSelectedDate();
+        }
+
+        private void moveMount(bool type)
+        {
+            if (type)
+            {
+                if (selectedMount == 12) { selectedMount = 1; moveYear(true); }
+                else selectedMount++;
+            }
+            else
+            {
+                if (selectedMount == 1) { selectedMount = 12; moveYear(false); }
+                else selectedMount--;
+            }
+        }
+
+        private void moveYear(bool type)
+        {
+            selectedYear = type ? selectedYear + 1 : selectedYear - 1;
+        }
+
+        private void addNoteInDay(int day)
+        {
+            wwf.appendNewNote(selectedYear,selectedMount,day,noteName.Text, noteTextBody.Text);
+            updateSelectedDayNotes(day);
+        }
+
+        private void addNote_Click(object sender, RoutedEventArgs e)
+        {
+            addNoteInDay(selectedDateTime.Day);
+            updateWeekend();
+            updateSelectedDayNotes(selectedDateTime.Day);
+        }
+
+        private void changeNote_Click(object sender, RoutedEventArgs e)
+        {
+            wwf.changeNote(selectedYear,selectedMount,selectedDateTime.Day, selectedNoteId, noteName.Text, noteTextBody.Text);
+            updateWeekend();
+            updateSelectedDayNotes(selectedDateTime.Day);
+
+        }
+
+        private void removeNote_Click(object sender, RoutedEventArgs e)
+        {
+            wwf.removeNote(selectedYear, selectedMount, selectedDateTime.Day, selectedNoteId);
+            updateWeekend();
+            updateSelectedDayNotes(selectedDateTime.Day);
+        }
+
+        private void selectedDayNotes_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            selectedNoteId = selectedDayNotes.SelectedIndex;
+            var n = wwf.getNoteAtDateByIdAsNote(selectedYear, selectedMount, selectedDateTime.Day, selectedNoteId);
+            if (n == null) return;
+            noteName.Text = n.name;
+            noteTextBody.Text = n.note;
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach(var n in wwf.getAllNotes())
+            {
+                sb.Append(n.ToString());
+                sb.Append("\n");
+            }
+            MessageBox.Show(sb.ToString());
         }
     }
 }
---
+++ WorkWithFile.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace WPFkalendarZpisnoy
+{
+    public class WorkWithFile
+    {
+        private static readonly string userFilePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+        string pathToFile;
+        string fileName;
+        XDocument loadedDoc = null;
+        XElement mainEl = null;
+
+        public class Note{
+            public readonly string name;
+            public readonly string note;
+            public readonly int id;
+            public readonly DateTime noteDate;
+            public Note(string name, string note, DateTime dateTime, int id)
+            {
+                this.name = name;
+                this.note = note;
+                this.noteDate = dateTime;
+                this.id = id;
+            }
+            public Note(string name, string note, int y, int m, int d, int id)
+            {
+                this.name = name;
+                this.note = note;
+                this.noteDate = new DateTime(y,m,d);
+                this.id = id;
+            }
+
+            public static Note prepareFromXMLE(XElement el)
+            {
+                if (el.Attribute("id") == null)
+                    throw new Exception("id tag equ null");
+                int _id = (int)el.Attribute("id");
+                string _name = el.Attribute("name") == null ? "" : el.Attribute("name").Value;
+                string _note = el.Attribute("note") == null ? "" : el.Attribute("note").Value;
+
+                int _y = el.Attribute("y") == null ? -1 : (int)el.Attribute("y");
+                int _m = el.Attribute("m") == null ? -1 : (int)el.Attribute("m");
+                int _d = el.Attribute("d") == null ? -1 : (int)el.Attribute("d");
+                if (_y == -1 | _m == -1 | _d == -1)
+                    throw new Exception("Date broken");
+
+                return new Note(_name,_note,_y,_m,_d,_id);
+            }
+
+            public override string ToString()
+            {
+                return noteDate.ToString("yyyy.MM.dd ")+this.name;
+            }
+        }
+        
+        public WorkWithFile(string pathToFile)
+        {
+            this.fileName = pathToFile + ".xml";
+            this.pathToFile = userFilePath + "\\kp330\\" + this.fileName;
+            init();
+        }
+
+        private void init()
+        {
+            if (!Directory.Exists(userFilePath + "\\kp330"))
+            {
+                Directory.CreateDirectory(userFilePath + "\\kp330");
+            }
+            XDocument mainDoc;
+            if (!File.Exists(this.pathToFile))
+            {
+                
+                mainDoc = new XDocument();
+                XElement element = new XElement("calendar");
+                mainDoc.Add(element);
+                mainDoc.Save(pathToFile);
+            }
+            mainDoc = XDocument.Load(this.pathToFile);
+            loadedDoc = mainDoc;
+            mainEl = loadedDoc.Root;
+        }
+
+        public void appendNewNote(int y, int m, int d, string name, string note) {
+            int countNoteOnDate = (from writedNote in this.mainEl.Elements() where areAttributesEqualesInt(writedNote, new string[] { "y", "m", "d" }, new int[] { y, m, d }) select writedNote).Count();
+            XElement xenote = new XElement("Note", 
+                new XAttribute("id", countNoteOnDate),
+                new XAttribute("y", y),
+                new XAttribute("m", m),
+                new XAttribute("d", d),
+                new XAttribute("name",name == "" ? "Записка №"+countNoteOnDate : name),
+                new XAttribute("note",note));
+            mainEl.Add(xenote);
+            loadedDoc.Save(this.pathToFile);
+        }
+
+        public void changeNote(int y, int m, int d, int id, string name, string noteText)
+        {
+            if (id == -1)
+                return;
+            XElement note = getNoteAtDateById(y, m, d, id);
+            if (note == null)
+                return;
+            note.Attribute("name").Value = name;
+            note.Attribute("note").Value = noteText;
+            loadedDoc.Save(this.pathToFile);
+        }
+
+        public void removeNote(int y, int m, int d, int id)
+        {
+            XElement note = getNoteAtDateById(y, m, d, id);
+            if (note == null)
+                return;
+            note.Remove();
+            foreach (var _note in findNotesAtDate(y, m, d))
+            {
+                if (_note.Attribute("id") == null)
+                    continue;
+                if ((int)_note.Attribute("id") > id)
+                    _note.SetAttributeValue("id", ((int)_note.Attribute("id"))-1);
+            }
+            loadedDoc.Save(this.pathToFile);
+        }
+
+        public IEnumerable<XElement> findNotesAtDate(int y, int m, int d)
+        {
+            return from writedNote in this.mainEl.Elements() where areAttributesEqualesInt(writedNote, new string[] { "y", "m", "d" }, new int[] { y, m, d }) select writedNote;
+        }
+        
+        public IEnumerable<Note> findNotesAtDateAsNote(int y, int m, int d)
+        {
+            List<Note> notes = new List<Note>();
+            foreach(var el in findNotesAtDate(y, m, d))
+            {
+                try
+                {
+                    var nel = Note.prepareFromXMLE(el);
+                    notes.Add(nel);
+                }
+                catch { continue; }
+            }
+            return notes;
+        }
+
+        public IEnumerable<XElement> findNotedDayAtMY(int y, int m)
+        {
+            return from writedNote in this.mainEl.Elements() where areAttributesEqualesInt(writedNote, new string[] { "y", "m" }, new int[] { y, m }) select writedNote;
+        }
+
+        public IEnumerable<int> findNumNotedDayAtMY(int y, int m)
+        {
+            return findNotedDayAtMY(y, m).Select(d => { return (int)d.Attribute("d"); });
+        }
+
+        public XElement getNoteAtDateById(int y, int m, int d, int id)
+        {
+            XElement note = null;
+            note = (from writedNote in this.mainEl.Elements() where areAttributesEqualesInt(writedNote, new string[] { "y", "m", "d", "id" }, new int[] { y, m, d, id }) select writedNote).First();
+            return note;
+        }
+
+        public Note getNoteAtDateByIdAsNote(int y, int m, int d, int id)
+        {
+            try
+            {
+                var nel = Note.prepareFromXMLE(getNoteAtDateById(y,m,d,id));
+                return nel;
+            }
+            catch { return null; }
+        }
+
+        public List<Note> getAllNotes()
+        {
+            return this.mainEl.Elements().Select(e=>Note.prepareFromXMLE(e)).OrderBy(e=>e.noteDate).ThenBy(e=>e.id).ToList();
+        }
+
+        public static bool areAttributesEqualesInt(XElement element, ICollection<string> attributesName, ICollection<int> attributesValue)
+        {
+            bool areEqu = true;
+            IEnumerator<string> enumeratorAN = attributesName.GetEnumerator();
+            IEnumerator<int> enumeratorAV = attributesValue.GetEnumerator();
+            for (int i = 0; i < Math.Min(attributesName.Count, attributesValue.Count); i++)
+            {
+                if (!(enumeratorAN.MoveNext() && enumeratorAV.MoveNext()))
+                    return i == 0 ? false : true;
+                areEqu &= (int)element.Attribute(enumeratorAN.Current) == enumeratorAV.Current;
+                if (!areEqu)
+                    return false;
+            }
+            return true;
+        }
+
+        public static bool areAttributeEqualeInt(XAttribute attribute, int value)
+        {
+            return ((int)attribute) == value;
+        }
+    }
+}
